3217b107a63c2501ae055335067fd79f4ec4019d
delacode <thivirudela@gmail.com>
2025-08-10 18:26:32 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 const CACHE = 'arclevel-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-180.png"
 ];
 
 self.addEventListener('install', e => {
